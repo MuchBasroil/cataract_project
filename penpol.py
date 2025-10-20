0226f49e484610ea163032a1d8e095a9d90b60ba
pascalreynaldy <reynaldye@gmail.com>
2025-10-20 12:02:50 +0700

    update paskul

--- penpol.py
+++ penpol.py
@@ -6,7 +6,7 @@ from PIL import Image
 # Load model
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model("cataract_mobilenetv2.h5")
+    model = tf.keras.models.load_model("best_model.h5")
     return model
 
 model = load_model()
